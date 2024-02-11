61b44fd29bd6bacafb0d5c519ee766ecc07b869c
captainplanet11 <michusingh5@gmail.com>
2024-02-11 23:54:03 +0530

    check

--- ch4/main.go
+++ ch4/main.go
@@ -1,9 +1,5 @@
 package main
 
-import (
-	"fmt"
-)
-
 // func main() {
 
 // 	x := 10
@@ -124,19 +120,21 @@ import (
 
 // }
 
-func main() {
-	words := []string{"a", "cow", "smile", "gopher", "octopus", "anthropoloigst"}
-	for _, word := range words {
-		switch size := len(word); size {
-		case 1, 2, 3, 4:
-			fmt.Println(word, "is a short word")
-		case 5:
-			wordLen := len(word)
-			fmt.Println(word, "is exactly the right length: ", wordLen)
-		case 6, 7, 8, 9:
-		default:
-			fmt.Println(word, "is a long word")
-		}
-	}
+// func main() {
+// 	words := []string{"a", "cow", "smile", "gopher", "octopus", "anthropoloigst"}
+// 	for _, word := range words {
+// 		switch size := len(word); size {
+// 		case 1, 2, 3, 4:
+// 			fmt.Println(word, "is a short word")
+// 		case 5:
+// 			wordLen := len(word)
+// 			fmt.Println(word, "is exactly the right length: ", wordLen)
+// 		case 6, 7, 8, 9:
+// 		default:
+// 			fmt.Println(word, "is a long word")
+// 		}
+// 	}
 
-}
+// }
+
+// go to yes gptp
